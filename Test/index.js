c010d49b04256ed8c80c30d2f39ce4846de6a8f1
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-12-21 11:53:23 -0300

    Curso de Node.js - Rotas #07

---
+++ Test/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+var titulo = "Bem Vindo ao Nosso Site";
+codigoBasico = "<!DOCTYPE html>" +
+"<html lang='pt-br'>" +
+"<head>" +
+    "<meta charset='UTF-8'>" + 
+    "<meta name='viewport' content='width=device-width, initial-scale=1.0'>" +
+    "<title>" + titulo + "</title>" +
+    "<script type='text/javascript' src=''></script>" +
+    "<script type='text/javascript' src='js/cpf.js'></script>" +
+"</head>" +
+"<body>" +
+    "<h1>Seja Bem Vindo ao Meu App.</h1>" +
+"</body>" +
+"</html>";
+
+app.get('/', function(req, res){
+    res.send(codigoBasico);
+});
+
+app.get("/sobre", function(req, res){
+    res.send("<title>Sobre O Site</title><h1>Informações Sobre O Nosso Site</h1>");
+});
+
+app.get("/blog", function(req, res) {
+    res.send("<title>Bem Vindo ao Nosso Blog</title><h1>Bem Vindo ao Nosso Blog</h1>");
+});
+
+
+
+
+
+
+
+
+
+app.listen(8081, function() {
+    console.log("Servidor Rodando na url http://localhost:8081.");
+});

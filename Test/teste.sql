93c4be07f66b304669e9a1da6a553ef8b88acac4
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-12-23 15:33:17 -0300

    Curso de Node.js - Manipulando o MySQL #13

--- Test/teste.sql
+++ Test/teste.sql
@@ -1,5 +1,11 @@
-CREATE TABLE usuarios(
+CREATE TABLE IF NOT EXISTS usuarios(
     nome VARCHAR(50),
     email VARCHAR(100),
     idade INT UNSIGNED
 );
+
+INSERT INTO usuarios(nome, email, idade) values(
+    "Clone Carlos Madureira de Carvalho ",
+    "clone_carlos@et.com", 
+    60
+);  

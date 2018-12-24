434880f271b12a7bbd50903d59bc0f75d6c76a88
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-12-24 11:37:23 -0300

    Curso de Node.js - Models no Sequelize #18

--- Test/test.js
+++ Test/test.js
@@ -4,8 +4,51 @@ const sequelize = new Sequelize('test', 'usuario1', '12345678', {
     dialect: 'mysql'
 });
 
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+});
+
+/*
+Postagem.create({
+    titulo: "UM TÍTULO QUALQUER",
+    conteudo: "UM CONTEÚDO QUALQUER"
+})
+*/
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+Usuario.create({
+    nome: "Victor",
+    sobrenome: "Lima",
+    idade: 20,
+    email: "blablabla@mail.com"
+})
+
+//Postagem.sync({force: true})
+//Usuario.sync({force: true})
+
+/*
 sequelize.authenticate().then(function() {
     console.log("Conectado com sucesso.");
 }).catch(function(erro){
     console.log("Falha ao conectar. Erro: " + erro);
 });
+*/

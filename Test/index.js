4b9342f202003cb0d6cbd584420715987864a1ce
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-12-25 10:44:01 -0300

    Curso de Node.js - Body parser #22

--- Test/index.js
+++ Test/index.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
-
-
+const bodyParser = require('body-parser');
 // Config
     // Template Engine
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
-
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 // Conexão com o banco de dados MySQL
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('test', 'usuario1', '12345678', {
     host: "localhost",
     dialect: 'mysql'
 });
-
 // Rotas
 app.get('/cad', function(req, res){
     res.render('formulario');
 })
 
 app.post('/add', function(req, res){
-    res.send('FORMULÁRIO RECEBIDO.');
+    res.send('Título:' + req.body.titulo + "; Conteúdo: " + req.body.conteudo);
 })
 
 

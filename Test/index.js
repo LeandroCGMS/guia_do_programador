f5702f6fc5fcd719bf7f06787e99699265cb2dcb
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-12-26 11:08:09 -0300

    Curso de Node.js - Deletando posts #26

--- Test/index.js
+++ Test/index.js
@@ -38,6 +38,13 @@ app.post('/add', function(req, res){
     })
 })
 
+app.get('/deletar/:id', function(req, res) {
+    Post.destroy({where: {'id': req.params.id}}).then(function(){
+        res.send("Postagem deletada com sucesso.")
+    }).catch(function(erro){
+        res.send("Falha ao deletar postagem. Erro: " + erro + ".")
+    })
+})
 
 app.listen(80, function() {
     console.log("Servidor Rodando na url http://localhost.");
